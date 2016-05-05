82b87c77f690a044f49c36d1cea065ac704b7735
Marty McLaughlin <martymclaugh@gmail.com>
2016-05-05 10:30:41 -0700

    changed all yes and no answers to read as y or n

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -15,4 +15,16 @@ health_insurance =
  puts "Would you like to enroll in the company’s health insurance?"
  health_insurance = gets.chomp
 
+if garlic_bread == "yes" || garlic_bread == "Yes" || garlic_bread == "y" || garlic_bread == "Y"
+	garlic_bread = "y"
+else
+	garlic_bread = "n"
+end
+
+if health_insurance == "yes" || health_insurance == "Yes" || health_insurance == "y" || health_insurance == "Y"
+	health_insurance = "y"
+else
+	health_insurance = "n"
+end
+
  puts age
