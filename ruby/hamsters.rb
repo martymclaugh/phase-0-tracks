ed55cd6511374e6bc598916facc409b7ab98d34a
Marty McLaughlin <martymclaugh@gmail.com>
2016-05-04 19:10:27 -0700

    made integer responses stored as integers

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -4,13 +4,13 @@ approx_age = nil
 puts "What's your hamsters name?"
 hamsters_name = gets.chomp
 puts "How loud is your hamster between 1-10?"
-volume_level = gets.chomp
+volume_level = gets.chomp.to_i
 puts "What's the fur color?"
 fur_color = gets.chomp
 puts "Is this hamster available for adoption"
 adoption = gets.chomp
 puts "Whats the approximate age of the hamster?"
-approx_age = gets.chomp
+approx_age = gets.chomp.to_f
 
 
 puts hamsters_name
