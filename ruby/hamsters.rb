e7ac26b19c55c7ae55225b73c2fae99824050b3d
Marty McLaughlin <martymclaugh@gmail.com>
2016-05-04 19:03:05 -0700

    created ruby program for adopting hamster

---
+++ ruby/hamsters.rb
@@ -0,0 +1,20 @@
+approx_age = nil
+
+
+puts "What's your hamsters name?"
+hamsters_name = gets.chomp
+puts "How loud is your hamster between 1-10?"
+volume_level = gets.chomp
+puts "What's the fur color?"
+fur_color = gets.chomp
+puts "Is this hamster available for adoption"
+adoption = gets.chomp
+puts "Whats the approximate age of the hamster?"
+approx_age = gets.chomp
+
+
+puts hamsters_name
+puts volume_level
+puts fur_color
+puts adoption
+puts approx_age
