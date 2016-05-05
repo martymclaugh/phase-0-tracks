dadaf101709afbf2171a85db77ed2a01b1bcf789
Marty McLaughlin <martymclaugh@gmail.com>
2016-05-04 23:06:36 -0700

    fixed when user input is nil on last question

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -10,12 +10,8 @@ fur_color = gets.chomp
 puts "Is this hamster available for adoption?"
 adoption = gets.chomp
 puts "Whats the approximate age of the hamster?"
-approx_age = gets.chomp
-if approx_age = int
-	approx_age.to_f 
-else
-	approx_age = nil
-end
+approx_age = Float(gets) rescue nil
+
 if adoption == "yes" || adoption == "Yes" || adoption == "y" || adoption == "Y"
 	adoption = "y"
 else
