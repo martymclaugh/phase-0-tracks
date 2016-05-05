b5afbf30b96e41773ea552e0c13fce13bf35a1c4
Marty McLaughlin <martymclaugh@gmail.com>
2016-05-04 19:31:04 -0700

    added two if statements and printing variables

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -7,14 +7,23 @@ puts "How loud is your hamster between 1-10?"
 volume_level = gets.chomp.to_i
 puts "What's the fur color?"
 fur_color = gets.chomp
-puts "Is this hamster available for adoption"
+puts "Is this hamster available for adoption?"
 adoption = gets.chomp
 puts "Whats the approximate age of the hamster?"
-approx_age = gets.chomp.to_f
+approx_age = gets.chomp
+if approx_age = int
+	approx_age.to_f 
+else
+	approx_age = nil
+end
+if adoption == "yes" || adoption == "Yes" || adoption == "y" || adoption == "Y"
+	adoption = "y"
+else
+	adoption = "n"
+end
 
-
-puts hamsters_name
-puts volume_level
-puts fur_color
-puts adoption
-puts approx_age
+puts "Hamester's Name = #{hamsters_name}"
+puts "Volume Level = #{volume_level}"
+puts "Fur Color = #{fur_color}"
+puts "Adoptable = #{adoption}"
+puts "Approximate Age = #{approx_age}"
