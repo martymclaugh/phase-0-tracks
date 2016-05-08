a62b13e021dd4d947b1cfe152a60457487caf28a
Marty McLaughlin <martymclaugh@gmail.com>
2016-05-08 12:50:25 -0700

    move comments to correct areas

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,36 +1,44 @@
 #Encrypt method:
-#- input: some string
-#- break up string into separate characters
-#- assign index to the characters
-#- add 1 to the index value of each letter in the string
-#- print the new index values together as a new string 
-
-#Decrypt method:
-#- assign index to the input string
-#- subtract 1 from the index value of each letter in string
-
+#- input: message
 def encrypt(message)
 	index = 0
-	while index < message.length 
+	#loop over index for amount in message length.
+	while index < message.length
+		#- assign index to the characters
+		#- add 1 to the index value of each letter in the string
 		message[index] = message[index].next
+		#add 1 to index counter  
 		index += 1
 	end
+	#- puts the new index values together as a new string 
+	#substitutes ab for a in special case of encrypting a "z"
 	message = message.gsub("ab","a")
 end
-
+#Decrypt method:
 def decrypt(mess)
+	#add alphabet to loop over
 	alphabet2 = "abcdefghijklmnopqrstuvwxyz"
+	#- assign index to the input string
 	index = 0
+	#add index for alphabet
 	i = 0
+	#blank space for output
 	output = ""
+	#loop index for message length
 	while index < mess.length
+		#compare message with corresponding alphabet index
 		if mess[index] == alphabet2[i]
+				#- subtract 1 from the index value of each letter in string
 				output += alphabet2[i - 1]
+			#add 1 to index counter
 			index += 1
+			#reset alphabet index to 0
 			i = 0
 		end
+		#add 1 to alphabet index counter
 		i += 1
 	end
+	#return output
 	output
 end
 
@@ -40,21 +48,24 @@ end
 # puts decrypt("afe")
 # puts decrypt(encrypt("swordfish"))
 # Our nested method call works because ruby reads the encrypt and then the decrypt due to the parentheses.
-# -ask user whether they would like to encrypt or decrypt a password
-# -ask them for the password
-# -conduct requested operation
-# - print result to screen
-#-exit the program
 
+#OUR DRIVER CODE
+
+# -ask user whether they would like to encrypt or decrypt a password
 puts "Would you like your password encryted or decrypted?"
 	input1 = gets.chomp
+	# -ask them for the password
 puts "What is your password"
 	password = gets.chomp
+	# -conduct requested operation
 	if input1 == "encrypted"
+		# - print result to screen
 		puts "Your encrypted password is #{encrypt(password)}"
 		else
+		# - print result to screen
 		puts "Your decrypted password is #{decrypt(password)}"
 	end
+#-exit the program
 exit
 
 
