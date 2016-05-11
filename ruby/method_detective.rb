e96576c878707c8539d5142029eeaaa081378df8
Marty McLaughlin <martymclaugh@gmail.com>
2016-05-10 18:55:32 -0700

    assign first half of methods

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,26 +4,28 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
+"iNvEsTiGaTiOn".swapcase
+=> “InVeStIgAtIoN”
 
-# "zom".<???>
-# => “zoom”
+"zom".insert(2,'o')
+=> “zoom”
 
-# "enhance".<???>
-# => "    enhance    "
+"enhance".ljust(10)
+"enhance".rjust(10)
+=> "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
-# => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
-#=> "the usual suspects"
+"Stop! You’re under arrest!".upcase
+=> "STOP! YOU’RE UNDER ARREST!"
 
-# " suspects".<???>
-# => "the usual suspects"
+"the usual".concat(" suspects")
+=> "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
-# => "The case of the disappearing last lette"
+" suspects".prepend("the usual")
+=> "the usual suspects"
+
+"The case of the disappearing last letter".chop
+=> "The case of the disappearing last lette"
 
 # "The mystery of the missing first letter".<???>
 # => "he mystery of the missing first letter"
