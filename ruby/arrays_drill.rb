a544a8121fda98d849f0c35163efa4a8d8364a5f
Marty McLaughlin <martymclaugh@gmail.com>
2016-05-11 14:43:11 -0700

    add if statement that finds something in array

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -13,3 +13,17 @@ p variable1
 variable1.insert(2, "C")
 
 p variable1
+
+variable1.shift
+
+p variable1
+
+if variable1.include?("b")
+	puts "yes it inlcudes b"
+else
+	puts "no it does not include b"
+end
+
+p variable1.include?("b")
+
+
