7af7bacb8afcae6eb8afacf9364a3cadf7fad31c
Marty McLaughlin <martymclaugh@gmail.com>
2016-05-13 16:42:01 -0700

    psuedocode a program using a hash

---
+++ ruby/hashes.rb
@@ -0,0 +1,10 @@
+
+# create keys for the client's name, age, number of children, decor theme, color theme, favorite furniture, has pets, cares about space to move around.
+# prompt user for information
+# convert user input to appropriate type
+# print hash back on screen when all questions are answered
+# give user opportunity to update any key
+# print latest version of hash
+interior_design = {}
+# create a new hash
+interior_design = {}
