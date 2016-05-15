eaebf6892c9193a4ab5a3b24bd3819b1aa100325
Marty McLaughlin <martymclaugh@gmail.com>
2016-05-15 13:30:29 -0700

    create hash with mixed array and hash contents

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,53 @@
+refrigerator = {
+	top_shelf: {
+		dairy: {
+			butter: 2,
+			yogurt: 8
+		},
+		sauces: [
+			'Mayo',
+			'Salad Dressing',
+			'Salsa'
+		]
+	},
+	second_shelf: {
+		leftovers: [
+			'Pot roast',
+			'Grilled Chicken',
+			'Green Beans',
+			'Salad'
+		],
+		eggs: {
+			hardboiled: 6,
+			untouched: 12
+		}
+	},
+	crisper_left: {
+		vegetables: {
+			bell_peppers: [
+				'Red',
+				'Green',
+				'Yellow'
+			],
+			lettuces: [
+				'Romaine',
+				'Iceburg',
+				'Organic Mixed Greens'
+			],
+			cucumbers: 4
+		},
+	crisper_right: {
+		fruits: {
+			grapes: [
+				'Red',
+				'Green'
+			],
+			apples: {
+				granny_smith: 4,
+				pink_lady: 6
+			},
+			organges: 5
+		},
+		lemon_juice: 1
+	}
+}
