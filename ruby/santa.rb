6bf4deddd3affe0a12cc0d40e24d202b42991d78
Marty McLaughlin <martymclaugh@gmail.com>
2016-05-19 13:07:00 -0700

    add loop that prints santa 'name' gender and ethnicity for each santa

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -8,7 +8,7 @@ class Santa
 	reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 
 	age = 0
-	
+
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
@@ -18,9 +18,24 @@ class Santa
 	end
 end
 
-santa = Santa.new
+# santa = Santa.new
 
-santa.speak
+# santa.speak
 
-santa. eat_milk_and_cookies("oreo")
+# santa. eat_milk_and_cookies("oreo")
 
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+  puts "There are now #{santas.length} in the group!"
+  puts "-----"
+end
+
+index = 0
+santas.each do 
+	puts "Santa#{index} is of the #{example_genders[index]} gender and #{example_ethnicities[index]} ethnicity."
+	puts "     "
+	index += 1
+end
