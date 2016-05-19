58416d33efe0fd32283ee88946e4c2dc476a5f40
Marty McLaughlin <martymclaugh@gmail.com>
2016-05-19 11:59:35 -0700

    define initialize method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -2,8 +2,12 @@ class Santa
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
-	
+
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie}!"
+	end
+
+	def initialize
+		puts "Initializing Santa instance ..."
 	end
 end
