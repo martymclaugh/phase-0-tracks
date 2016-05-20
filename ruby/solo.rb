674d1bb3639cfa07d8f0cb9bdeb5ae7daa3c4b86
Marty McLaughlin <martymclaugh@gmail.com>
2016-05-20 12:27:41 -0700

    ask user how many zombies they want to create

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -8,9 +8,9 @@ class Zombie
 	# 	movement speed
 	# 	age
 
-	def initialize(age)
+	def initialize
 		puts "Initializing zombie instance..."
-		@age = age
+		@age = 1
 		@brains_eaten = 0
 		@movement_speed = "slow"
 	end
@@ -28,16 +28,32 @@ class Zombie
 		puts "The zombie has taken a bite out of your #{limb}!"
 		if limb == "head"
 			@brains_eaten += 1
+			puts "The zombie has now eaten #{brains_eaten} brains!"
 		end
 	end
 
 	def survivor_spotted
 		@movement_speed = "fast"
+		puts "The zombie has seen you and is now moving really #{movement_speed}!"
+	end
+
+	def eat_brains(num_brains)
+		@brains_eaten += num_brains
+		puts "The zombie has now eaten #{brains_eaten} brains!"
 	end
 end
 
+puts "Welcome to the apocolypse, how many zombies would you like to create?"
+num_of_zombies = gets.chomp.to_i
+horde = []
+p num_of_zombies.times do
+	horde << Zombie.new
+end
+p horde
+puts "You now have #{}"
+
 #DRIVER CODE
-fred = Zombie.new(10)
+fred = Zombie.new
 
 puts fred.brains_eaten
 
@@ -51,7 +67,8 @@ fred.survivor_spotted
 
 puts fred.movement_speed
 
-
+fred.eat_brains(12)
+fred.eat_brains(15)
 
 
 
