afa0bf3b857295427cfee45393c63fb088f9bc6c
Marty McLaughlin <martymclaugh@gmail.com>
2016-05-20 17:57:52 -0700

    finish ui for zombie creator

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,7 +1,7 @@
 # create a zombie class
 class Zombie
-	attr_reader :age
-	attr_accessor :brains_eaten, :movement_speed, :name
+	attr_reader :style
+	attr_accessor :brains_eaten, :movement_speed, :name, :age
 
 	# create 3 attribute classes
 	# 	brains eaten
@@ -10,10 +10,11 @@ class Zombie
 
 	def initialize
 		puts "Initializing zombie instance..."
-		@age = rand(365)
-		@brains_eaten = rand(30)
+		@age = age
+		@brains_eaten = 0
 		@movement_speed = "slow"
 		@name = name
+		@style = "classy"
 	end
 
 	# create 3 methods
@@ -26,9 +27,9 @@ class Zombie
 	# 		prints the zombie ate your brain
 
 	def bite_appendage(limb)
-		puts "The zombie has taken a bite out of your #{limb}!"
+		puts "The zombie has taken a bite out of #{limb}!"
 		if limb == "head"
-			@brains_eaten += 1
+			brains_eaten += 1
 			puts "The zombie has now eaten #{brains_eaten} brains!"
 		end
 	end
@@ -39,7 +40,7 @@ class Zombie
 	end
 
 	def eat_brains(num_brains)
-		@brains_eaten += num_brains
+		@brains_eaten += num_brains.to_i
 		puts "The zombie has now eaten #{brains_eaten} brains!"
 	end
 
@@ -58,8 +59,10 @@ input = ""
 num_of_zoms = horde.length
 index = 0
 until input == "done"
-	puts "Welcome to the Build-a-Horde workshop. Your horde currently has #{num_of_zoms} zombies."
+	#intro
+	puts "Welcome to the Build-a-Horde workshop. Your horde currently has #{index} zombies."
 	puts " "
+	#prompt user for name
 	puts "Type the name of your next zombie or type 'done' if you are finished."
 	name_input = gets.chomp
 	if name_input == "done"
@@ -67,8 +70,22 @@ until input == "done"
 	end
 	horde << Zombie.new
 	horde[index].name = name_input
-	p horde
+	puts "-----"
+	#prompt user for age
+	puts "How many days old is #{horde[index].name}?"
+	horde[index].age = gets.chomp
+	puts "-----"
+	#prompt user for speed
+	puts "How fast is #{horde[index].name}?"
+	horde[index].movement_speed = gets.chomp
+	puts "-----"
+	#prompt user for amount of brains eaten
+	puts "How many brains has #{horde[index].name} eaten"
+	num_brain = gets.chomp
+	horde[index].eat_brains(num_brain)
+	horde[index].brains_eaten = num_brain
 	index += 1
+	puts "-----"
 end
 
 
