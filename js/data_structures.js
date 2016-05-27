f8ec2bfdacefb1c6d42675737a859639931844d5
Marty McLaughlin <martymclaugh@gmail.com>
2016-05-27 13:54:19 -0700

    create object and loop through object

--- js/data_structures.js
+++ js/data_structures.js
@@ -12,3 +12,38 @@ for (var i = 0; i < colors.length; i++) {
 	horses[names[i]] = colors[i];
 }
 console.log(horses);
+
+function Car(year, make, model) {
+	console.log("Our new car:", this);
+	this.year = year;
+	this.make = make;
+	this.model = model;
+
+	this.start = function() { console.log("Vroom!"); };
+}
+
+console.log("Let's build a new car...");
+
+var newCar = new Car(2006, "Subaru", "WRX");
+
+console.log(newCar);
+
+newCar.start();
+
+var anotherCar = new Car(2016, "BMW", "3 series");
+
+console.log(anotherCar);
+
+anotherCar.start();
+
+for (var key in newCar) {
+	var obj = newCar[key]
+	console.log(key + ": " + obj);
+}
+
+
+
+
+
+
+
