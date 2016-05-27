725d3049dc80bfef134061f4fca8438828abaa11
Marty McLaughlin <martymclaugh@gmail.com>
2016-05-27 13:32:10 -0700

    create object with names and colors

--- js/data_structures.js
+++ js/data_structures.js
@@ -5,3 +5,10 @@ colors.push("blue");
 names.push("eric");
 console.log(colors);
 console.log(names);
+
+var horses = {};
+
+for (var i = 0; i < colors.length; i++) {
+	horses[names[i]] = colors[i];
+}
+console.log(horses);
