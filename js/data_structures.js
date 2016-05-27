f2905547b9cd50792fd71814f8a3b883ebcef0dc
Marty McLaughlin <martymclaugh@gmail.com>
2016-05-27 13:16:27 -0700

    create two arrays

---
+++ js/data_structures.js
@@ -0,0 +1,7 @@
+var colors = ["gray", "red", "orange", "black"];
+var names = ["george", "charlie", "max", "larry"];
+
+colors.push("blue");
+names.push("eric");
+console.log(colors);
+console.log(names);
