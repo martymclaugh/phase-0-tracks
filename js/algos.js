c9c791dd92fcc3f708c164c549abbe4312077c09
Marty McLaughlin <martymclaugh@gmail.com>
2016-05-28 13:46:16 -0700

    change code to have a long phrase variable instead of an array.

--- js/algos.js
+++ js/algos.js
@@ -1,21 +1,25 @@
-// create empty array that will contain the longest phrase.
-// iterrate through array of phrases and store them in the empty array.
-	// if the phrase is longer than the phrase in the empty array, do nothing.
-	// otherwise add the phrase to the array and delete the phrase at index 0
-	// return the only phrase in the array.
+
+// create length variable to record length of phrases
+// create return phrase variable
+// create for statement to itterate through the arrays
+	// if the phrase is longer than the current length of the length variable, change the length variable to the length of that phrase.
+	// change the return phrase variable to that phrase
+	// return the 'return phrase variable'
 var array = ["long phrase", "even longer phrase", "longest phrase ever dude"];
 var secondarray = ["11111", "2222222", "333333333333"]
+
 function longestPhrase(list) {
 	this.list = list;
-	var longphrase = []
+	var length = 0;
+	var longphrase;
 	for (var i = 0; i < list.length; i++) {
-		if (list[i].length > longphrase[i].length){
-			longphrase.push(list);
-			longphrase.splice (0, 1);
+		if (list[i].length > length){
+			var length = list[i].length
+			longphrase = list[i];
 		}
 	}
-	return longphrase[0];
+	return longphrase
 }
-
+// DRIVER CODE
 console.log(longestPhrase(array));
 console.log(longestPhrase(secondarray));
