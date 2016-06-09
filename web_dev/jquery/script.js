ff8c71c0bff11badc8280a1d9c7fdf1ee6e13ebd
Marty McLaughlin <martymclaugh@gmail.com>
2016-06-09 12:38:52 -0700

    make each paragraph hide upon clicking

---
+++ web_dev/jquery/script.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+	$("#p1").click(function(){
+		$("#p1").fadeOut("slow");
+	});
+});
+
+$(document).ready(function(){
+	$("#p2").click(function(){
+		$("#p2").fadeOut("slow");
+	});
+});
+
+$(document).ready(function(){
+	$("#p3").click(function(){
+		$("#p3").fadeOut("slow");
+	});
+});
+
+$(document).ready(function(){
+	$("#p4").click(function(){
+		$("#p4").fadeOut("slow");
+	});
+});
+
+$(document).ready(function(){
+	$("#p5").click(function(){
+		$("#p5").fadeOut("slow");
+	});
+});
+
+$(document).ready(function(){
+	$("#p6").click(function(){
+		$("#p6").fadeOut("slow");
+	});
+});
+
+$(document).ready(function(){
+	$("#p7").click(function(){
+		$("#p7").fadeOut("slow");
+	});
+});
+
+$(document).ready(function(){
+	$("#p8").click(function(){
+		$("#p8").fadeOut("slow");
+	});
+});
