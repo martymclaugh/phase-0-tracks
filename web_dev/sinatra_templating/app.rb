e2ae5d33a57ef326bb1574050332be792fc5b427
Marty McLaughlin <martymclaugh@gmail.com>
2016-06-11 16:24:06 -0700

    add teachers database and route for new teachers

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -7,10 +7,20 @@ set :public_folder, File.dirname(__FILE__) + '/static'
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
 
+db2 = SQLite3::Database.new("teachers.db")
+db2.results_as_hash = true
 # show students on the home page
 get '/' do
+	@teachers = db2.execute("SELECT * FROM teachers")
   @students = db.execute("SELECT * FROM students")
   erb :home
+end
+
+get '/teachers' do
+end
+
+get '/teachers/new' do
+	erb :new_teacher
 end
 
 get '/students/new' do
